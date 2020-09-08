4d11a7cc2d120db6cfd163aa2747dfb5397791f4
cupsarestupid <cupsarestupid@gmail.com>
2020-09-07 23:40:41 -0500

    added to plan added to struct Attributes

--- Project Cute/main.cpp
+++ Project Cute/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <vector>
 #include <string>
 #include <iomanip>
+
+/*attributes:
+Strength
+Constitution
+Dexterity
+intelligence
+Wisdom
+Charisma
+*/
+
+struct Attributes{
+  int strength;
+  int constitution;
+  int dexterity;
+  int intelligence;
+  int wisdom;
+  int charisma;
+};
+
+int main(){
+  Attributes 
+}
