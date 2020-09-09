ed817b7718c9613ea2b2b0427bc4612b1a9b90cf
cupsarestupid <cupsarestupid@gmail.com>
2020-09-08 22:10:20 -0500

    Brainstorming how to link classes to choice menu

--- Project Cute/main.cpp
+++ Project Cute/main.cpp
@@ -15,6 +15,7 @@ struct Attributes{
   int wisdom;
   int charisma;
 };
+
 //Sections of the program
 //char caracterMenu();
 char characterClassSelection(std::string);
@@ -25,8 +26,8 @@ int main(){
   //Attributes character;
   std::string name, gender;
   char classChoice, menuChoice;
-  //std::vector <std::string> optionalClasses;
-
+  /*std::vector <std::string> optionalClasses = {Space Barbarian, Space Bard, Space Cleric, Space Druid, Space Fighter, Space Monk, Space Paladin, Space Ranger};
+  std::vector <char>*/
   //char class;
   //short level;
 
@@ -36,59 +37,69 @@ int main(){
   std::cout << "Enter gender:\n";
   getline(std::cin, gender);
 
-  classChoice = characterClassSelection(name);
+  while(true){
+    classChoice = characterClassSelection(name);
+    if(classChoice == 'J' || classChoice == 'j'){
+      break;
+    }
+  }
 
   return 0;
 }
-//Menu
+
 char characterClassSelection(std::string name){
   char select;
 
-  std::cout << "Select your class " << name << "\n";
-  std::cout << "\nA) Space Barbarian\n";
-  std::cout << "B) Space Bard\n";
-  std::cout << "C) Space Cleric\n";
-  std::cout << "D) Space Druid\n";
-  std::cout << "E) Space Fighter\n";
-  std::cout << "F) Space Monk\n";
-  std::cout << "G) Space Paladin\n";
-  std::cout << "H) Space Ranger\n";
-  std::cout << "\nJ) Back\n";
-  std::cin >> select;
-  switch(select){
-    case 'A':
-    case 'a':
-      return select;
-      break;
-    case 'B':
-    case 'b':
-      return select;
-      break;
-    case 'C':
-    case 'c':
-      return select;
-      break;
-    case 'D':
-    case 'd':
-      return select;
-      break;
-    case 'E':
-    case 'e':
-      return select;
-      break;
-    case 'F':
-    case 'f':
-      return select;
-      break;
-    case 'G':
-    case 'g':
-      return select;
-      break;
-    case 'H':
-    case 'h':
-      return select;
-      break;
-    default :
-      std::cout << "Invalid choice, make another selection";
+  while(true){
+    std::cout << "Select your class " << name << "\n";
+
+    std::cout << "\nA) Space Barbarian\n";
+    std::cout << "B) Space Bard\n";
+    std::cout << "C) Space Cleric\n";
+    std::cout << "D) Space Druid\n";
+    std::cout << "E) Space Fighter\n";
+    std::cout << "F) Space Monk\n";
+    std::cout << "G) Space Paladin\n";
+    std::cout << "H) Space Ranger\n";
+
+    std::cout << "\nJ) Exit\n";
+
+    std::cin >> select;
+    switch(select){
+      case 'A':
+      case 'a':
+        return select;
+        break;
+      case 'B':
+      case 'b':
+        return select;
+        break;
+      case 'C':
+      case 'c':
+        return select;
+        break;
+      case 'D':
+      case 'd':
+        return select;
+        break;
+      case 'E':
+      case 'e':
+        return select;
+        break;
+      case 'F':
+      case 'f':
+        return select;
+        break;
+      case 'G':
+      case 'g':
+        return select;
+        break;
+      case 'H':
+      case 'h':
+        return select;
+        break;
+      default :
+        std::cout << "Invalid choice, make another selection";
+      }
+    }
   }
-}
