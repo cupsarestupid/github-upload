a51947c56a4c96f9cefbb256a45d5354703b9a4f
cupsarestupid <cupsarestupid@gmail.com>
2020-09-09 20:34:47 -0500

    added class select call out and attribute header

---
+++ Project Cute/Attributes.h
@@ -0,0 +1,13 @@
+#ifndef ATTRIBUTES_H
+#define ATTRIBUTES_H
+
+class Attributes{
+public:
+  int strength;
+  int constitution;
+  int dexterity;
+  int intelligence;
+  int wisdom;
+  int charisma;    
+};
+#endif
--- Project Cute/main.cpp
+++ Project Cute/main.cpp
@@ -1,51 +1,48 @@
 /*The idea is like a DnD type thing but with a different concept*/
 #include <iostream>
-#include <fstream>
-#include <cmath>
-#include <vector>
 #include <string>
-#include <iomanip>
+#include "Attributes.h"
 
-//Attributes to designate
-struct Attributes{
-  int strength;
-  int constitution;
-  int dexterity;
-  int intelligence;
-  int wisdom;
-  int charisma;
-};
-//literal classes
+
 class SpaceBarbarian{
   public:
+
 };
 
 class SpaceBard{
   public:
+
 };
 
 class SpaceCleric{
   public:
+
 };
 
 class SpaceDruid{
   public:
+
+
 };
 
 class SpaceFighter{
   public:
+
 };
 
 class SpaceMonk{
   public:
+
 };
 
 class SpacePaladin{
   public:
+
 };
 
 class SpaceRanger{
   public:
+
 };
 
 //Sections of the program
@@ -57,10 +54,7 @@ int characterLevel();*/
 int main(){
   //Attributes character;
   std::string name, gender;
-  char classChoice, menuChoice;
-  /*std::vector <std::string> optionalClasses = {Space Barbarian, Space Bard, Space Cleric, Space Druid, Space Fighter, Space Monk, Space Paladin, Space Ranger};
-  std::vector <char>*/
-  //char class;
+  char classSelect, menuChoice;
   //short level;
 
   std::cout << "Enter chosen name:\n";
@@ -70,10 +64,8 @@ int main(){
   getline(std::cin, gender);
 
   while(true){
-    classChoice = characterClassSelection(name);
-    if(classChoice == 'J' || classChoice == 'j'){
+    classSelect = characterClassSelection(name);
       break;
-    }
   }
 
   return 0;
@@ -94,44 +86,52 @@ char characterClassSelection(std::string name){
     std::cout << "G) Space Paladin\n";
     std::cout << "H) Space Ranger\n";
 
-    std::cout << "\nJ) Exit\n";
+    std::cout << "\nClass Choice : ";
 
     std::cin >> select;
     switch(select){
       case 'A':
       case 'a':
+        std::cout << "\nA Space Barbarian ...\n";
         return select;
         break;
       case 'B':
       case 'b':
+        std::cout << "\nA Space Bard ...\n";
         return select;
         break;
       case 'C':
       case 'c':
+        std::cout << "\nA Space Cleric ...\n";
         return select;
         break;
       case 'D':
       case 'd':
+        std::cout << "\nA Space Druid ...\n";
         return select;
         break;
       case 'E':
       case 'e':
+        std::cout << "\nA Space Fighter ...\n";
         return select;
         break;
       case 'F':
       case 'f':
+        std::cout << "\nA Space Monk ...\n";
         return select;
         break;
       case 'G':
       case 'g':
+        std::cout << "\nA Space Paladin ...\n";
         return select;
         break;
       case 'H':
       case 'h':
+        std::cout << "\nA Space Ranger ...\n";
         return select;
         break;
       default :
-        std::cout << "Invalid choice, make another selection";
+        std::cout << "\nInvalid choice, make another selection";
       }
     }
   }
