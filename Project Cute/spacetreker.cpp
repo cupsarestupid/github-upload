194036bbb87abee773aa7856f4aabca324dc0cb1
cupsarestupid <cupsarestupid@gmail.com>
2020-09-12 18:05:05 -0500

    Screen centering

--- Project Cute/spacetreker.cpp
+++ Project Cute/spacetreker.cpp
@@ -1,5 +1,6 @@
-/*The idea is like a DnD type thing but with a different concept, please read "Plan.txt"*/
+/* spacetrecker The idea is like a DnD type thing but with a different concept, please read "Plan.txt"*/
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include "Attributes.h"
 
@@ -53,17 +54,24 @@ int main(){
   std::string name, gender;
   char classSelect, menuChoice;
 
-  std::cout << "Enter chosen name:\n";
+  std::cout << std::right << std::setw(50) << "Enter chosen name:\n";
+  std::cout << std::right << std::setw(48);
   getline(std::cin, name);
 
   std::cout << "Enter gender:\n";
   getline(std::cin, gender);
+  system("clear");
+  //std::cout << "\033[2J\033[1;1H";
 
+  std::cout << "Ah yes, I can see that you are a " << gender << "\n";
+  std::cout << "Well then " << name << ", your class? \n";
 
   while(true){
     classSelect = characterClassSelection(name, gender);
       break;
   }
+
+  std::cout << "\nWhat say we roll some stats?";
 
   return 0;
 }
@@ -72,13 +80,7 @@ char characterClassSelection(std::string name, std::string gender){
   char select;
 
   while(true){
-    for (int i = 0; i < 10; i++){
-      std::cout << "\n\n\n\n\n\n\n\n\n\n";
-    }
-    std::cout << "Ah yes, I can see that you are " << gender << "\n";
-    std::cout << "Well then " << name << ", your class? \n";
-
-    std::cout << "\nA) Space Barbarian\n";
+    std::cout << "A) Space Barbarian\n";
     std::cout << "B) Space Bard\n";
     std::cout << "C) Space Cleric\n";
     std::cout << "D) Space Druid\n";
@@ -93,46 +95,55 @@ char characterClassSelection(std::string name, std::string gender){
     switch(select){
       case 'A':
       case 'a':
-        std::cout << "\nA Space Barbarian ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Barbarian ...\n";
         return select;
         break;
       case 'B':
       case 'b':
-        std::cout << "\nA Space Bard ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Bard ...\n";
         return select;
         break;
       case 'C':
       case 'c':
-        std::cout << "\nA Space Cleric ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Cleric ...\n";
         return select;
         break;
       case 'D':
       case 'd':
-        std::cout << "\nA Space Druid ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Druid ...\n";
         return select;
         break;
       case 'E':
       case 'e':
-        std::cout << "\nA Space Fighter ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Fighter ...\n";
         return select;
         break;
       case 'F':
       case 'f':
-        std::cout << "\nA Space Monk ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Monk ...\n";
         return select;
         break;
       case 'G':
       case 'g':
-        std::cout << "\nA Space Paladin ...\n";
+      system("clear");
+        std::cout << "Ah...\nA Space Paladin ...\n";
         return select;
         break;
       case 'H':
       case 'h':
-        std::cout << "\nA Space Ranger ...\n";
+        system("clear");
+        std::cout << "Ah...\nA Space Ranger ...\n";
         return select;
         break;
       default :
-        std::cout << "\nInvalid choice, make another selection";
+        system("clear");
+        std::cout << "\nInvalid choice, make another selection\n";
       }
     }
   }
