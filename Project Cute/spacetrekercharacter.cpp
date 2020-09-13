9bf4ef5896046398e1d7af9bc904e6dfda135aa4
cupsarestupid <cupsarestupid@gmail.com>
2020-09-13 09:50:37 -0500

    Moved characters to character project, renamed main to into file

---
+++ Project Cute/spacetrekercharacter.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Attributes.h"
+
+class SpaceBarbarian{
+  public:
+
+};
+
+class SpaceBard{
+  public:
+
+};
+
+class SpaceCleric{
+  public:
+
+};
+
+class SpaceDruid{
+  public:
+
+
+};
+
+class SpaceFighter{
+  public:
+
+};
+
+class SpaceMonk{
+  public:
+
+};
+
+class SpacePaladin{
+  public:
+
+};
+
+class SpaceRanger{
+  public:
+
+};
